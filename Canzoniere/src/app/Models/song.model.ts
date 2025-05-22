3129121e20e27541c7c69a92b34a42962aa9e594
Seba-33 <159021649+Seba-33@users.noreply.github.com>
2025-05-22 13:51:21 +0000

    terzo punto

--- Canzoniere/src/app/Models/song.model.ts
+++ Canzoniere/src/app/Models/song.model.ts
@@ -1,3 +1,12 @@
 export class Song {
-    
+    datauscita: string
+    durata: string
+    title: string
+
+    constructor(datauscita: string, durata: string, title: string){
+        this.datauscita = datauscita
+        this.durata = durata
+        this.title = title
+
+    }
 }
--- Canzoniere/src/app/app.component.ts
+++ Canzoniere/src/app/app.component.ts
@@ -1,12 +1,39 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'Canzoniere';
+  loading!: boolean
+  obs!: Observable<object>
+  data!: object
+  http!: HttpClient
+
+  constructor(http: HttpClient){this.http = http}
+
+  makeChiamata(){
+    this.loading = true
+    this.obs = this.http.get('https://my-json-server.typicode.com/malizia-g/hotel/short-songlist')
+    this.obs.subscribe(this.getData)
+    
+  }
+
+  getData = (d: object) => {
+    this.data = d
+    console.log(this.data)
+
+  }
+
+  ngOnInit(): void {
+    this.makeChiamata()
+  }
+
 }
